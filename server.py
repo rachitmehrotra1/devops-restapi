01bdf880f314598a5398ffb8ba89c263e4dbc836
carlos-guzman <guzmca@gmail.com>
2016-10-29 16:52:26 -0400

    Add basic meet functionality (#8)

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ def index():
 @app.route('/users/<id>', methods=['DELETE'])
 def delete_users(id):
     global users
-    users = get_from_redis('users') 
+    users = get_from_redis('users')
     if not users.has_key(id):
         return reply({ 'error' : 'User %s doesn\'t exist' % id }, HTTP_400_BAD_REQUEST)
     del users[id];
@@ -52,6 +52,88 @@ def create_user():
         redis_server.set('users',json_users)
 
     return reply(message, rc)
+
+@app.route('/meet', methods=['GET'])
+def meet():
+    global users
+    users = json.loads(redis_server.get('users'))
+    ids = request.args.get('users').split(',')
+    if any([not users.has_key(x) for x in ids]):
+        return reply({ 'error' : 'Enter valid existing user ids'}, HTTP_400_BAD_REQUEST)
+    single_schedules = map(lambda _id: [(x['from'], x['to'], [_id])
+                                        for x in users[_id]['times']],
+                            ids)
+    final_schedule = reduce(merge, single_schedules)
+    # Return the times where most people can meet
+    max_people = len(max(final_schedule, key=lambda x: len(x[2]))[2])
+    final_schedule = filter(lambda x: len(x[2])==max_people, final_schedule)
+    final_schedule.sort()
+    json_schedule = [{"from": x[0], "to": x[1], "people": x[2]}
+                     for x in final_schedule]
+    return reply(json_schedule, HTTP_200_OK)
+
+def merge(sched1, sched2):
+    sched1.sort()
+    sched2.sort()
+    final_sched = []
+    processed = {}
+    i1 = 0
+    i2 = 0
+    while i1 < len(sched1) and i2 < len(sched2):
+        while i1 < len(sched1) and sched1[i1][1] < sched2[i2][0]:
+            final_sched.append(sched1[i1])
+            i1 += 1
+        # If they don't intersect, append the rest
+        if i1 == len(sched1):
+            final_sched.extend(sched2[i2:])
+            return final_sched
+
+        while i2 < len(sched2) and sched2[i2][1] < sched1[i1][0]:
+            final_sched.append(sched2[i2])
+            i2 += 1
+        # If they don't intersect, append the rest
+        if i2 == len(sched2):
+            final_sched.extend(sched1[i1:])
+            return final_sched
+
+        # We know they intersect
+        final_sched.extend(merge2(sched1[i1], sched2[i2]))
+        i1 += 1
+        i2 += 1
+
+    #Remove elements that have length 0 time
+    return filter(lambda x: x[0] < x[1], final_sched)
+
+# TODO Fix algorithm, since it doesn't work when an interval intersects with more than one intervals of the other person
+# e.g.
+# merge([(1, 10, [1]), (12, 14, [1])], [(5, 15, [2])])
+
+def merge2(sched1, sched2):
+    final_sched = []
+    first = min(sched1, sched2)
+    second = max(sched1, sched2)
+    final_sched.append((first[0],
+                        second[0],
+                        first[2]))
+    if min(first[1], second[1]) == first[1]:
+    # first   |--------|
+    # second       |-------|
+        final_sched.append((second[0],
+                            first[1],
+                            second[2] + first[2]))
+        final_sched.append((first[1],
+                            second[1],
+                            second[2]))
+    else:
+    # first   |--------|
+    # second     |---|
+       final_sched.append((second[0],
+                           second[1],
+                           second[2] + first[2]))
+       final_sched.append((second[1],
+                           first[1],
+                           first[2]))
+    return final_sched
 
 def reply(message, rc):
     response = Response(json.dumps(message))
