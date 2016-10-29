a25a2b5acf0390f0c69309e5c89ad1ffb44883e1
Rachit Mehrotra <rachitmehrotra@gmail.com>
2016-10-28 22:21:25 -0400

    Same for deleting a user

--- server.py
+++ server.py
@@ -9,6 +9,11 @@ users = [{"id": 0, "name": "Carlos Guzman", "times":[{"from":1477523957, "to":14
 def index():
     return jsonify(name='Meeting REST API', version='1.0', url='/'), 200
 
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_users(id):
+    del users[id];
+    return '', HTTP_204_NO_CONTENT
+
 @app.route('/users', methods=['GET'])
 def list_users():
     return reply(users, 200)
